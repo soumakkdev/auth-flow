a77adc5be117ab34fd6b6bd47adfcac1c2fc4d40
soumakkdev <soumakkdutta@gmail.com>
2024-12-06 00:58:42 +0530

    password strength bar added

--- apps/client/src/components/input/PasswordField.tsx
+++ apps/client/src/components/input/PasswordField.tsx
@@ -7,15 +7,28 @@ import { Label } from '../ui/label'
 interface PasswordFieldProps extends React.ComponentProps<'input'> {
 	label?: string
 	showLockIcon?: boolean
+	showStrength?: boolean
 }
 
+const passwordMaxStrength = 5
+
 const PasswordField = forwardRef<HTMLInputElement, PasswordFieldProps>(
-	({ label, className, showLockIcon, id, ...props }, ref) => {
+	({ label, className, showLockIcon, showStrength, id, ...props }, ref) => {
 		const [passwordVisible, setPasswordVisible] = useState(false)
 
+		const password = props.value as string
+		const strength = checkStrength(password)
+
 		return (
-			<div className="space-y-2">
-				{label ? <Label htmlFor={id}>{label}</Label> : null}
+			<div className="space-y-3">
+				<div className="flex items-center justify-between">
+					{label ? <Label htmlFor={id}>{label}</Label> : null}
+
+					{showStrength ? (
+						<span className="text-xs text-muted-foreground">{getStrengthText(strength)}</span>
+					) : null}
+				</div>
+
 				<div className="relative">
 					<Input
 						ref={ref}
@@ -42,9 +55,53 @@ const PasswordField = forwardRef<HTMLInputElement, PasswordFieldProps>(
 						)}
 					</div>
 				</div>
+
+				{showStrength ? (
+					<div className="h-1 w-full bg-zinc-200 rounded-full">
+						<div
+							className={cn(
+								'h-full rounded-full transition-all duration-500',
+								getStrengthColor(strength)
+							)}
+							style={{
+								width: `${(strength / passwordMaxStrength) * 100}%`,
+							}}
+						></div>
+					</div>
+				) : null}
 			</div>
 		)
 	}
 )
 
 export default PasswordField
+
+function checkStrength(password: string) {
+	let score = 0
+	if (password) {
+		if (/[a-z]/.test(password)) score += 1
+		if (/[A-Z]/.test(password)) score += 1
+		if (/[0-9]/.test(password)) score += 1
+		if (/.{8}/.test(password)) score += 1
+		if (/[!@#$%^&*_-]/.test(password)) score += 1
+	}
+	return score
+}
+
+function getStrengthColor(score: number) {
+	if (score === 0) return 'bg-zinc-200'
+	else if (score === 1) return 'bg-red-500'
+	else if (score === 2) return 'bg-orange-500'
+	else if (score === 3) return 'bg-amber-500'
+	else if (score === 4) return 'bg-yellow-500'
+	else if (score === 5) return 'bg-green-500'
+}
+
+function getStrengthText(score: number) {
+	if (score === 0) return 'Too short'
+	else if (score === 1) return 'Weak'
+	else if (score === 2) return 'Fair'
+	else if (score === 3) return 'Good'
+	else if (score === 4) return 'Good'
+	else if (score === 5) return 'Strong'
+}
--- apps/client/src/pages/Signup.tsx
+++ apps/client/src/pages/Signup.tsx
@@ -1,10 +1,32 @@
 import InputField from '@/components/input/InputField'
 import PasswordField from '@/components/input/PasswordField'
 import { Button } from '@/components/ui/button'
-import { Label } from '@/components/ui/label'
+import { useForm } from '@tanstack/react-form'
 import { Mail, User2 } from 'lucide-react'
+import { FormEvent } from 'react'
 
 export default function Signup() {
+	const form = useForm({
+		defaultValues: {
+			email: '',
+			password: '',
+		},
+		onSubmit: async ({ value }) => {
+			console.log(value)
+			try {
+				// console.log(res)
+			} catch (error) {
+				console.error(error)
+			}
+		},
+	})
+
+	function handleSubmit(e: FormEvent) {
+		e.preventDefault()
+		e.stopPropagation()
+		form.handleSubmit()
+	}
+
 	return (
 		<div>
 			<div className="mb-12">
@@ -14,7 +36,7 @@ export default function Signup() {
 				</p>
 			</div>
 
-			<div className="space-y-6 max-w-[440px] mx-auto">
+			<form onSubmit={handleSubmit} className="space-y-6 max-w-[440px] mx-auto">
 				<InputField id="name" label="Name" placeholder="Enter your name" startIcon={<User2 size={16} />} />
 
 				<InputField
@@ -24,17 +46,25 @@ export default function Signup() {
 					startIcon={<Mail size={16} />}
 				/>
 
-				<div className="space-y-3">
-					<div className="flex justify-between items-center">
-						<Label htmlFor="password">Password</Label>
-					</div>
-					<PasswordField id="password" placeholder="Enter the password" showLockIcon />
-				</div>
+				<form.Field name="password">
+					{(field) => (
+						<PasswordField
+							label="Password"
+							placeholder="Enter the password"
+							showLockIcon
+							showStrength
+							id={field.name}
+							value={field.state.value}
+							onBlur={field.handleBlur}
+							onChange={(e) => field.handleChange(e.target.value)}
+						/>
+					)}
+				</form.Field>
 
 				<Button size="lg" className="w-full">
 					Cerate account
 				</Button>
-			</div>
+			</form>
 
 			<p className="text-sm text-center mt-8">
 				Already have an account?{' '}
